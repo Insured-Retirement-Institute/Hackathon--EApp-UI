0dcfcd29213577daee2c190302fc92d67bdeed49
jakegalligan <galligan1996@gmail.com>
2025-02-11 12:23:34 -0500

    submission component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { QuestionnaireComponent } from './components/questionnaire/questionnaire.component';
+import { SubmissionComponent } from './components/submission/submission.component';
 
 export const appRoutes: Routes = [
     {
@@ -17,5 +18,10 @@ export const appRoutes: Routes = [
         title: 'Questionnaire',
         path: 'questionnaire',
         component: QuestionnaireComponent,
+    },
+    {
+        title: 'Submission',
+        path: 'submission',
+        component: SubmissionComponent,
     }
 ];
---
+++ src/app/components/submission/submission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission',
+  imports: [],
+  templateUrl: './submission.component.html',
+  styleUrl: './submission.component.scss'
+})
+export class SubmissionComponent {
+
+}
