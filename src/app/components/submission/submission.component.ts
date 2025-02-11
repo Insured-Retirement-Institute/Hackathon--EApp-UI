5eb7147a0cf33c34afdb5652f7e5197603cb2e15
jakegalligan <galligan1996@gmail.com>
2025-02-11 16:09:41 -0500

    update

--- src/app/components/submission/submission.component.ts
+++ src/app/components/submission/submission.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RecommendationApiService } from '../../services/recommendation-api';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-submission',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './submission.component.html',
   styleUrl: './submission.component.scss'
 })
