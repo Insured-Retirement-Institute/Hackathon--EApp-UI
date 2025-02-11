7a39afc4e5dfb01f72d3d2f06f7fcd7e7e1d1a0c
jakegalligan <galligan1996@gmail.com>
2025-02-11 14:12:01 -0500

    state

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
+import { RecommendationApiService } from '../../services/recommendation-api';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,13 @@ import { MatSliderModule } from '@angular/material/slider';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit { 
+
+
+  constructor(private recommendationsService: RecommendationApiService) { }
+  ngOnInit(): void {
+    this.recommendationsService.getRecommendations('jj').subscribe((response) => {
+      console.log(response);
+    });
+  }
+}
