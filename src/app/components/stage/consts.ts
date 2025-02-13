a66f6fc1f361ba6ceafe617982c66ff9c832bb2d
jakegalligan <galligan1996@gmail.com>
2025-02-12 21:53:17 -0500

    trim list

--- src/app/components/stage/consts.ts
+++ src/app/components/stage/consts.ts
@@ -14,17 +14,10 @@ export const hardcodedAllocations: PreBuiltAllocation[] = [
         Percentage: 0
     },
     {
-        Name: 'BlackRock® Adaptive US Equity 7% Index, 2-Year Point to Point with PR',
-        Percentage: 0
-    },
-    {
         Name: 'BNPP Patriot Technology Index Annual Point to Point with PR',
         Percentage: 0
     },
-    {
-        Name: 'BNPP Patriot Technology Index 2-Year Point to Point with PR',
-        Percentage: 0
-    },
+
     {
         Name: 'S&P 500® Dividend Aristocrats® Daily Risk Control 5% ER Index, Annual Point to Point with PR',
         Percentage: 0
@@ -36,13 +29,5 @@ export const hardcodedAllocations: PreBuiltAllocation[] = [
     {
         Name: 'S&P 500® Index, Annual Point to Point with Cap',
         Percentage: 0
-    },
-    {
-        Name: 'S&P 500® Index, Annual Point to Point with PR',
-        Percentage: 0
-    },
-    {
-        Name: 'S&P 500® Index, Monthly Point to Point with Cap',
-        Percentage: 0
-    },
+    }
 ]
